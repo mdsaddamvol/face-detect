2402a93e066228340acadafe2f34f7fa767c4a5f
mdsaddamvol <mdsaddamvoltas@gmail.com>
2021-04-27 21:32:49 +0600

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.40fafd4a32e6e326637f8d56706a6083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6cee16e7ce756f93bd5d4a1ca06b6e",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "56ca8e4a33f4ec3d3cfd",
+    "url": "http://localhost:3000/static/css/main.dd98ade2.chunk.css"
+  },
+  {
+    "revision": "7a8681ea7ae116850606",
+    "url": "http://localhost:3000/static/js/2.3c685b10.chunk.js"
+  },
+  {
+    "revision": "4f5f623cc9239300338c8ee43b7de73c",
+    "url": "http://localhost:3000/static/js/2.3c685b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ca8e4a33f4ec3d3cfd",
+    "url": "http://localhost:3000/static/js/main.31a6840f.chunk.js"
+  },
+  {
+    "revision": "63b58d05e038d24f7a71",
+    "url": "http://localhost:3000/static/js/runtime-main.5e465d15.js"
+  }
+]);
