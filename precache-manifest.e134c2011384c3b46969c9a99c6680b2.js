a2b757cc4f3774e2b7a68a0e315ddc408456c72f
mdsaddamvol <mdsaddamvoltas@gmail.com>
2021-04-28 12:13:11 +0600

    Updates

---
+++ precache-manifest.e134c2011384c3b46969c9a99c6680b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb5446749576a7771334d1e57172b76",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "003e9d510122b9130a6e",
+    "url": "http://localhost:3000/static/css/main.315aab48.chunk.css"
+  },
+  {
+    "revision": "3556f92d059494d9496e",
+    "url": "http://localhost:3000/static/js/2.003061ab.chunk.js"
+  },
+  {
+    "revision": "4f5f623cc9239300338c8ee43b7de73c",
+    "url": "http://localhost:3000/static/js/2.003061ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003e9d510122b9130a6e",
+    "url": "http://localhost:3000/static/js/main.02060611.chunk.js"
+  },
+  {
+    "revision": "63b58d05e038d24f7a71",
+    "url": "http://localhost:3000/static/js/runtime-main.5e465d15.js"
+  }
+]);
